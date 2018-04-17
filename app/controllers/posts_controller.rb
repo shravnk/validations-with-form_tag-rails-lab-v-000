baba3ef9dde85f5411f6b01e576a39d6a011fd4e
Brendan O'Dea <brendan.ode4@gmail.com>
2018-04-17 21:05:54 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new 
+    @post = Post.new
+  end
+
   def update
     @post = Post.find(params[:id])
 
